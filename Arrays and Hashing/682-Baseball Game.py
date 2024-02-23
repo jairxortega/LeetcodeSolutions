3fd61c8d59a6f85c84be4f1f09b5c74953b37f3c
jairxortega <104592853+jairxortega@users.noreply.github.com>
2024-02-23 11:09:47 -0600

    682

---
+++ Arrays and Hashing/682-Baseball Game.py
@@ -0,0 +1,18 @@
+# Easy
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = [] # initialize an empty stack
+
+        for ops in operations: # iterate through the operations
+            if ops == 'C':
+                stack.pop() # remove the last element from the stack
+            elif ops == 'D':
+                stack.append(stack[-1] * 2) # append the last element of the stack multiplied by 2
+            elif ops == '+':
+                stack.append(stack[-1] + stack[-2]) # append the sum of the last two elements of the stack
+            else:
+                stack.append(int(ops)) # append the integer value of the string to the stack
+        return sum(stack) # return the sum of the stack
+
+# Time complexity: O(n)
+# The run time is O(n) because we are only iterating through the operations list once.
